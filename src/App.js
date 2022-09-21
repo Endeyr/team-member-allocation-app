d6891abd177528e2dee0fe4cd8e7f633ad1cbc7d
Endeyr <aaron.thompson42594@gmail.com>
2022-09-21 13:15:26 -0400

    added functional components, bootstrap 5 css, employee cards, drop down team list, and state events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Employees from './Employees'
+import Footer from './Footer'
+import Header from './Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [selectedTeam, setTeam] = useState('TeamB')
+
+	const [employees, setEmployees] = useState([
+		{
+			id: 1,
+			fullName: 'Bob Jones',
+			designation: 'Javascript Developer',
+			gender: 'male',
+			teamName: 'TeamA',
+		},
+		{
+			id: 2,
+			fullName: 'Jill Bailey',
+			designation: 'Node Developer',
+			gender: 'female',
+			teamName: 'TeamB',
+		},
+		{
+			id: 3,
+			fullName: 'Gail Shepherd',
+			designation: 'Java Developer',
+			gender: 'female',
+			teamName: 'TeamC',
+		},
+		{
+			id: 4,
+			fullName: 'Sam Reynolds',
+			designation: 'React Developer',
+			gender: 'male',
+			teamName: 'TeamD',
+		},
+		{
+			id: 5,
+			fullName: 'Bob Jones',
+			designation: 'Javascript Developer',
+			gender: 'male',
+			teamName: 'TeamA',
+		},
+		{
+			id: 6,
+			fullName: 'Jill Bailey',
+			designation: 'Node Developer',
+			gender: 'female',
+			teamName: 'TeamB',
+		},
+		{
+			id: 7,
+			fullName: 'Gail Shepherd',
+			designation: 'Java Developer',
+			gender: 'female',
+			teamName: 'TeamC',
+		},
+		{
+			id: 8,
+			fullName: 'Sam Reynolds',
+			designation: 'React Developer',
+			gender: 'male',
+			teamName: 'TeamD',
+		},
+		{
+			id: 9,
+			fullName: 'Bob Jones',
+			designation: 'Javascript Developer',
+			gender: 'male',
+			teamName: 'TeamA',
+		},
+		{
+			id: 10,
+			fullName: 'Jill Bailey',
+			designation: 'Node Developer',
+			gender: 'female',
+			teamName: 'TeamB',
+		},
+		{
+			id: 11,
+			fullName: 'Gail Shepherd',
+			designation: 'Java Developer',
+			gender: 'female',
+			teamName: 'TeamC',
+		},
+		{
+			id: 12,
+			fullName: 'Sam Reynolds',
+			designation: 'React Developer',
+			gender: 'male',
+			teamName: 'TeamD',
+		},
+	])
+
+	function handleTeamSelectionChange(event) {
+		setTeam(event.target.value)
+	}
+
+	function handleEmployeeCardClick(event) {
+		const transformedEmployees = employees.map((employee) =>
+			employee.id === parseInt(event.currentTarget.id)
+				? employee.teamName === selectedTeam
+					? { ...employee, teamName: '' }
+					: { ...employee, teamName: selectedTeam }
+				: employee
+		)
+		setEmployees(transformedEmployees)
+	}
+
+	return (
+		<div className="App">
+			<Header />
+			<Employees
+				employees={employees}
+				selectedTeam={selectedTeam}
+				handleEmployeeCardClick={handleEmployeeCardClick}
+				handleTeamSelectionChange={handleTeamSelectionChange}
+			/>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
